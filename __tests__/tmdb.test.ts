2301dc48d7fd75eb655ba968f9ad59bd7fc121ad
Steve Ho <steve.ho@ffw.com>
2023-12-09 13:49:22 +0100

    update unit test

--- __tests__/tmdb.test.ts
+++ __tests__/tmdb.test.ts
@@ -1,17 +1,10 @@
 // jest implementation of the api
-import axios from "axios";
-import { fetchMovies, fetchMovie } from "../core/adapters/tmdb";
+import { getMedia } from "@/lib/services/tmdb.api";
 import { describe } from "node:test";
 
 describe("data-access", () => {
-    it("fetchMovies", async () => {
-        const data = await fetchMovies(1);
-        expect(data).toBeDefined();
-        // expect(data.status).toBe(200);
-    });
-
-    it("fetchMovie", async () => {
-        const data = await fetchMovie(1075794);
+    it("getMedia", async () => {
+        const data = await getMedia("movie", "1");
         expect(data).toBeDefined();
         // expect(data.status).toBe(200);
     });
