cbb6b2fb5d217e6dba06475bc62191d8dfdbc217
Steve Ho <steve.ho@ffw.com>
2023-12-09 13:50:40 +0100

    add auth components

---
+++ components/auth/NextAuthProvider.tsx
@@ -0,0 +1,10 @@
+"use client";
+import exp from "constants";
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+const NextAuthProvider = ({ children }: { children: ReactNode }) => {
+  return process.env.NEXT_PUBLIC_NEXTAUTH ? <SessionProvider>{children}</SessionProvider> : <>{children}</>;
+};
+
+export default NextAuthProvider;
