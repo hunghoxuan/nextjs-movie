f33056111a8e125d61c605eb372e3f99682b1f52
Steve Ho <steve.ho@ffw.com>
2023-12-11 08:54:47 +0100

    fix not display username bug

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,11 +1,11 @@
-"use client";
-
 import { MenuProps } from "@/lib/types/web";
 import NavbarVertical from "@/components/layout/navbar/NavbarVertical";
 import NavbarTop from "@/components/layout/navbar/NavbarTop";
+import { getAuth } from "@/lib/auth";
 
-export default function Navbar( props : MenuProps ) {
+export default async function Navbar( props : MenuProps ) {
+  const { user, session } = await getAuth();
   return (
-    props.orientation === "horizontal" ? <NavbarTop { ...props } /> : <NavbarVertical { ...props } />
+    props.orientation === "horizontal" ? <NavbarTop user={user} { ...props } /> : <NavbarVertical user={user} { ...props } />
   );
 }
